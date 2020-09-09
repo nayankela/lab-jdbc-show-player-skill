732a1bfa346ff2402252b9ef3ae35d8ee1b6ce16
nayankela <nayankela09@gmail.com>
2020-09-09 17:59:34 +0530

    done

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,26 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	
+	public static void main(String[] args) throws SQLException {
+		//Skill skill = new Skill();
+		SkillDAO skillDAO = new SkillDAO();
+		
+		 List<Skill> list= skillDAO.listAllSkills();
+		 
+		 System.out.println("---------------------------");
+		 System.out.println("SkillId     SkillName");
+		 System.out.println("---------------------------");
+		 for(Skill skill2 : list) {
+			 System.out.print(+skill2.getSkilld()+"            "+ skill2.getSkillName());
+			 System.out.println();
+		 }
+		 System.out.println("---------------------------");
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,41 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+
+	List<Skill> skills = new ArrayList<Skill>();
+
+	public List<Skill> listAllSkills() throws SQLException {
+		String sql = "SELECT * FROM SKILL ORDER BY NAME ASC";
+		Connection con = null;
+
+		try {
+			con = ConnectionManager.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Statement statement = con.createStatement();
+		ResultSet resultSet = statement.executeQuery(sql);
+
+		while (resultSet.next()) {
+			Long skilld = resultSet.getLong(1);
+			String skillName = resultSet.getString(2);
+
+			Skill skill = new Skill(skilld, skillName);
+			skills.add(skill);
+		}
+
+		return skills;
+
+	}
+}
